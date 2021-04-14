978648a6ccc858c468e75d2e52074f8c4c6ded37
Loyd Rafols <4452378+ljrafols@users.noreply.github.com>
2021-04-13 22:44:16 -0400

    Initial release version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# securityonion-virustotal-hash
-Python script that integrates the VT API into SO-extracted executables
+# Security Onion - VirusTotal API integration script (get-vt-rating)
+
+get-vt-rating is a Python script that creates alerts based on stats provided by the VirusTotal API.
+
+The script iteratively loops through all files extracted by Zeek (by default, it checks the default SO extracted directory at `/nsm/zeek/extracted/`). Each file that is encountered has an MD5 hash generated and submitted to the VirusTotal API. The results from the API request are used to determine if a file is malicious or not (this is currently set to at least 10 AV engines resulting in "malicious"). In the current iteration, it only logs to the console and to the "`virustotal.log`" file in the CWD, but I plan on integrating it into Security Onion's Alerts dashboard.
+
+To test this script, I have provided 5 malware samples in the `malware-samples/` directory, courtesy of dasmalwerk.eu:
+- **Gen:Variant.Johnnie.97338** (240387329dee4f03f98a89a2feff9bf30dcba61fcf614cdac24129da54442762.zip)
+- **Trojan.GenericKD.40436037** (785872bbef35d86fe6ce8a53be29995cfd0f251d2a171145bd6685bebe63ebc8.zip)
+- **'Adware ( 004f7c2e1 )** (37ea273266aa2d28430194fca27849170d609d338abc9c6c43c4e6be1bcf51f9.zip)
+- **Trojan.Delf.Agent.HZ** (bf34c8ed9467299cb2c7d711e63ab460e4039d5355ef76eb1d5c73b51b0ef637.zip)
+- **Gen:Heur.PonyStealer.2** (42154d0805933548da9b7a9fbbce40be9e155091e6f96ed4ce324c21b3430b20.zip)
+
+One **non-malicious** executable was also tested to verify that the script would not incorrectly classify malicious executables; it was not included in this repo. It is the `firefox.exe` executable from a standard **Mozilla Firefox 87.0 (64-bit)** installation.
+
+Tested and working on Security Onion 2.3.40. 
+
+# Usage
+`python get-vt-rating.py`
+
+# Requirements
+- Security Onion 2.3.40
+- aiohttp (installed automatically through requirements.txt or install manually via "pip install aiohttp")
+- gcc, python3-devel (yum install gcc python3-devel)
+
+# License
+Licensed under GNU GPL v3, found at https://www.gnu.org/licenses/gpl-3.0.txt.
+
+[VirusTotal/vt-py](https://github.com/VirusTotal/vt-py/blob/master/LICENSE): Apache License 2.0
---
+++ get-vt-rating.py
@@ -0,0 +1,91 @@
+"""
+Name: Get VT Rating
+Author: Loyd Rafols (ljrafols/lessigen)
+Date Created: Apr 6, 2021
+Date Modified: Apr 7, 2021
+
+Reqs: gcc and python3-devel (yum install gcc python3-devel), aiohttp (python3 -m pip install aiohttp)
+
+Licensed under the GNU General Public License Version 3 (GNU GPL v3),
+    available at: https://www.gnu.org/licenses/gpl-3.0.txt
+
+vt-py library is licensed under Apache License 2.0, see LICENSE for more info
+"""
+
+import os
+import time
+import hashlib
+import vt
+
+
+def main():
+    # Change this dir string to the directory of the extracted files
+    directory = os.fsencode("/nsm/zeek/extracted/complete")
+
+    for file in os.listdir(directory):
+        path = os.path.join(directory, file)
+        
+        if os.path.isdir(path):
+            pass        # skip other subdirectories
+        else:
+            filename = os.fsdecode(path)
+            md5hash = retrieve_file_hash(filename)
+
+            vt_result = query_vt_api(md5hash)       # query VT API using hash
+            is_hash_malicious(vt_result, filename, md5hash)
+            
+def retrieve_file_hash(filename):
+    """Reads a given file's (filename) contents and generates an MD5 hash.
+    
+    Returns the MD5 hash of the file as a string.
+    """
+
+    with open(filename, "rb") as f:
+        exe_bytes = f.read()
+        md5hash = hashlib.md5(exe_bytes).hexdigest()
+        f.close()
+
+    return md5hash
+
+def query_vt_api(md5hash):
+    """Queries the VirusTotal API using a file's hash (md5hash).
+    
+    Returns the vt_result variable as an Object.
+    """
+
+    # ENTER YOUR API KEY BELOW
+    vt_api = vt.Client("<YOUR-API-KEY-HERE>")
+    vt_result = vt_api.get_object("/files/{hash}".format(hash = md5hash))
+    vt_api.close()
+
+    return vt_result
+
+def is_hash_malicious(vt_result, filename, md5hash):
+    """Determines if a hash submitted to VirusTotal is malicious or not.
+    Currently set for a minimum of 10 AV engines to have detected the sample as "malicious".
+    Logs resultant filename and hash into "virustotal.log" in the CWD, along with their determined maliciousness.
+
+    Does not return anything.
+    """
+    malicious_count = vt_result.last_analysis_stats.get("malicious")      # get number of malicious matches
+
+    with open("virustotal.log", "a") as vt_log:     # write result to virustotal.log file
+        if (malicious_count >= 10):       # tune for individual taste
+            vt_log.write("!! ALERT !! - VirusTotal determined this file and hash to be malicious --\nFilename: " +
+                filename + ";\nHash: "+ md5hash + "\n")
+            print("!! ALERT !! - VirusTotal determined this file and hash to be malicious: " 
+                + filename + "; " + md5hash)
+
+        else:
+            vt_log.write("Following file was not detected as malicious by VirusTotal --\nFilename: " +
+                filename + ";\nHash: "+ md5hash + "\n")
+            print("Not malicious: " + filename + "; " + md5hash)
+        
+        vt_log.close()
+
+    # poor boi can't afford VT premium API :(
+    # mitigate likelihood of VT API rate limiting
+    time.sleep(15) 
+
+if __name__ == '__main__':
+    main()
